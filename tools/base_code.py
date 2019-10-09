f6228ffcf6b3e1baa5ca8d460db853a9346510dc
housonZhou <1733776802@qq.com>
2019-10-09 15:57:42 +0800

    删除冗余函数，更新其他函数新写法

--- tools/base_code.py
+++ tools/base_code.py
@@ -2,39 +2,27 @@ import xlrd
 import openpyxl
 import json
 import os
+from collections import defaultdict
+
+import pandas as pd
 
 
 class BaseCode(object):
 
-    @classmethod
-    def count_list_items(cls, data_list):
+    @staticmethod
+    def count_list_items(data_list):
         '''
         统计一个列表的元素出现的次数
         :param data_list: 待解析的list
         :return: {1:[a, b], 2:[c], 3:[d,e]}
         '''
-        data_json = {}
-        new_v = {}
+        data_json = defaultdict(lambda: 0)
+        new_v = defaultdict(lambda: [])
         for item in data_list:
-            if item in data_json:
-                data_json[item] += 1
-            else:
-                data_json[item] = 1
+            data_json[item] += 1
         for k, v in data_json.items():
-            if v in new_v:
-                new_v[v].append(k)
-            else:
-                new_v[v] = [k]
-        return new_v
-
-    def _get_file_from_dir(self, dir_path, file_list, file_type):
-        for item in os.listdir(dir_path):
-            now_path = os.path.join(dir_path, item)
-            if os.path.isfile(now_path) and now_path.endswith(file_type):
-                file_list.append(now_path)
-            elif os.path.isdir(now_path):
-                self._get_file_from_dir(now_path, file_list, file_type)
-        return file_list
+            new_v[v].append(k)
+        return dict(new_v)
 
     def get_file_from_dir(self, dir_path, file_type="wav"):
         '''
@@ -45,127 +33,76 @@ class BaseCode(object):
         '''
         return self._get_file_from_dir(dir_path, [], file_type)
 
-    @classmethod
-    def get_data_from_json(cls, json_file, encoding="utf8"):
+    def _get_file_from_dir(self, dir_path, file_list, file_type):
+        for item in os.listdir(dir_path):
+            now_path = os.path.join(dir_path, item)
+            if os.path.isfile(now_path) and now_path.endswith(file_type):
+                file_list.append(now_path)
+            elif os.path.isdir(now_path):
+                self._get_file_from_dir(now_path, file_list, file_type)
+        return file_list
+
+    @staticmethod
+    def get_data_from_json(json_file, encoding="utf8"):
         with open(json_file, "r", encoding=encoding)as f:
             return [json.loads(line.strip()) for line in f]
 
-    @classmethod
-    def get_data_from_txt(cls, txt_file, encoding="utf8"):
+    @staticmethod
+    def get_data_from_txt(txt_file, encoding="utf8"):
         with open(txt_file, "r", encoding=encoding)as f:
             return [line.strip() for line in f]
 
-    @classmethod
-    def generator_list(cls, list_in, count):
-        for i in range(count):
-            num = (i % len(list_in))
-            yield list_in[num]
+    @staticmethod
+    def cut_list_by_count(list_in, e_count):
+        num = len(list_in) // e_count
+        num = num if len(list_in) % e_count == 0 else num + 1
+        return [list_in[i * e_count:(i + 1) * e_count] for i in range(num)]
 
-    def get_data_from_list_by_count(self, list_in, e_count):
-        re_list = []
-        gn = self.generator_list(list_in, e_count)
-        for i in range(e_count):
-            re_list.append(next(gn))
-        return re_list
+    @staticmethod
+    def json2excel(all_json, save_path):
+        df = pd.DataFrame(all_json)
+        df.to_excel(save_path)
 
-    @classmethod
-    def from_json_build_excel(cls, all_json, save_path, need_head):
-        '''
-        将格式化的json数据保存成excel文件
-        :param all_json: [{key1:value1, key2:value2}, {key1:value1, key2:value2}, {}, {}...]
-        :param save_path: 保存的excel文件路径
-        :param need_head: 待保存的json数据的key 如保存所有：[i for i in all_json[0]]
-        :return: None
-        '''
-        wb = openpyxl.Workbook()
-        ws = wb.get_active_sheet()
-        for i in range(len(need_head)):
-            ws.cell(row=1, column=1 + i).value = need_head[i]
-        row = 2
-        for item in all_json:
-            for i in range(len(need_head)):
-                ws.cell(row=row, column=1 + i).value = item.get(need_head[i])
-            row += 1
-        wb.save(save_path)
-
-    def from_all_json_build_excel(self, data_json, save_path):
-        self.from_json_build_excel(data_json, save_path, [i for i in data_json[0]])
+    @staticmethod
+    def excel2json(excel_path, orient='records'):
+        df = pd.read_excel(excel_path)
+        return df.to_dict(orient=orient)
 
 
-class Analysis(object):
-    """
-    读取工具类
-    """
+class Append2Excel:
+    def __init__(self, excel_path):
+        self.excel_path = excel_path
+        self.csv_path = self.excel_path.replace('.xlsx', '.csv')
+        self._need_head = True
 
-    def __init__(self, path="file.xlsx"):
-        self.filepath = path
+    def add(self, data: list):
+        df = pd.DataFrame(data)
+        df.to_csv(self.csv_path, mode='a', encoding='utf8', index=False, header=self._need_head)
+        self._need_head = False
 
-    def open_excel(self, file='file.xlsx'):
-        try:
-            data = xlrd.open_workbook(file)
-            return data
-        except Exception:
-            print("打开文件异常")
+    @property
+    def need_head(self):
+        return self._need_head
 
-    def excel_table_byindex(self, file, colnameindex=0, by_index=0, start_row=1):
-        data = self.open_excel(file)
-        table = data.sheets()[by_index]
-        nrows = table.nrows  # 行数
-        ncols = table.ncols  # 列数
-        colnames = table.row_values(colnameindex)  # 某一行数据
-        list = []
-        for rownum in range(start_row, nrows):
-            row = table.row_values(rownum)
-            if row:
-                app = {}
-                for i in range(len(colnames)):
-                    app[colnames[i]] = row[i]
-                list.append(app)
-        return list
+    @need_head.setter
+    def need_head(self, need):
+        if isinstance(need, bool):
+            self._need_head = need
+        else:
+            print('need 类型必须为bool')
 
-    def excel_table_byname(self, file, colnameindex=0, by_name='Sheet1', start_row=1):
-        data = self.open_excel(file)
-        table = data.sheet_by_name(by_name)
-        nrows = table.nrows
-        colnames = table.row_values(colnameindex)
-        list = []
-        for rownum in range(start_row, nrows):
-            row = table.row_values(rownum)
-            if row:
-                app = {}
-                for i in range(len(colnames)):
-                    app[colnames[i]] = row[i]
-                list.append(app)
-        return list
+    def __del__(self):
+        if os.path.exists(self.csv_path):
+            df = pd.read_csv(self.csv_path)
+            df.to_excel(self.excel_path, index=False)
 
-    def excel_table_byrow(self, file, by_index=0, start_row=0):
-        data = self.open_excel(file)
-        table = data.sheets()[by_index]
-        list = []
-        nrows = table.nrows
-        for rownum in range(start_row, nrows):
-            row = table.row_values(rownum)
-            if row:
-                list.append(row)
-        return list
 
-    def openpy_excel_bycol(self, file, by_row=0, colnameindex=0):
-        wb = openpyxl.load_workbook(file)
-        ws = wb.active
-        max_rows = ws.max_row
-        max_cols = ws.max_column
-        list = []
-        for col_index in range(colnameindex, max_cols + 1):
-            obj_list = []
-            for row_index in range(by_row, max_rows + 1):
-                obj_list.append(ws.cell(row=row_index, column=col_index).value)
-            list.append(obj_list)
-        return list
+def test():
+    ae = Append2Excel(r'C:\Users\17337\houszhou\data\SpiderData\test_excel.xlsx')
+    ae.need_head = False
+    print(ae.need_head)
 
 
 if __name__ == '__main__':
-    # pass
-    ana = Analysis()
-    excel_path = r'C:\Users\17337\Downloads\W020190530357290617892.xls'
-    for data in ana.excel_table_byrow(excel_path):
-        print(data)
+    bc = BaseCode()
+    test()
